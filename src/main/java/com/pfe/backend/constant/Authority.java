e63976e94d0177908d7be5dc6f8fe2933122657c
wassim <baklouti.wassim@esprit.tn>
2024-10-25 23:38:21 +0100

    cors config

--- src/main/java/com/pfe/backend/constant/Authority.java
+++ src/main/java/com/pfe/backend/constant/Authority.java
@@ -8,7 +8,7 @@ public class Authority {
     public static final String[] ADMIN_AUTHORITIES = { "user:read", "user:create", "user:update", "user:delete" };
 }
 /*public class Authority {
-    public static final String[] USER_AUTHORITIES = { "user:read" };
+
     public static final String[] AUDITOR_AUTHORITIES = { "user:read" };
     public static final String[] MANAGER_AUTHORITIES = { "user:read", "user:update" };
     public static final String[] DATA_SCIENTIST_AUTHORITIES = {"user:read","user:update" };
